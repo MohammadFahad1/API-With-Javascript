83efcd5c592dcecd3db9ff8c30838e431836572b
MohammadFahad1 <fahad4bangladesh@gmail.com>
2022-08-29 23:02:59 +0600

    Get Country Details On Click of a Button

--- js/countries.js
+++ js/countries.js
@@ -17,14 +17,27 @@ const displayCountries = (countries) => {
         countryDiv.innerHTML = `
             <h3>Country Name: ${country.name.common}</h3>
             <p>Capital: ${country.capital ? country.capital[0] : 'No Capital'}</p>
-            <button onclick='loadCountryDetail()'>Details</button>
+            <button onclick='loadCountryDetail("${country.cca2}")'>Details</button>
         `;
         countriesContainer.appendChild(countryDiv);
     });
 }
 
-const loadCountryDetail = () => {
-    console.log('Get Contry Detail');
+const loadCountryDetail = (code) => {
+    // https://restcountries.com/v3.1/alpha/pe
+    const url = `https://restcountries.com/v3.1/alpha/${code}`;
+    // console.log('Get Contry Detail', code);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountryDetail(data[0]))
+}
+
+const displayCountryDetail = country => {
+    const countryDetail = document.getElementById('country-detail');
+    countryDetail.innerHTML = `
+        <h2>Detail: ${country.name.common}</h2>
+        <img src="${country.flags.png}">
+    `;
 }
 
 loadCountries();
